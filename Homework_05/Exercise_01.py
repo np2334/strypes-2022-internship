903767feefcb04598d19a521a767c2cd8cf8edca
np2334 <nikolay_pavlov01@outlook.com>
2022-07-17 22:37:39 +0300

    Homework 05

--- Homework_05/Exercise_01.py
+++ Homework_05/Exercise_01.py
@@ -1,5 +1,5 @@
 from Exercise_01_classes import Bill, BillBatch, CashDesk
-
+print("Testing class Bill")
 a = Bill(10)
 b = Bill(5)
 c = Bill(10)
@@ -19,7 +19,7 @@ if c in money_holder:
 
 print(money_holder) 
 
-print("\n\n\n\n")
+print("\n\n\n\nTesting class BillBatch")
 
 values = [10, 20, 50, 100]
 bills = [Bill(value) for value in values]
@@ -27,7 +27,7 @@ batch = BillBatch(bills)
 for bill in batch:
     print(bill)
 
-print("\n\n\n\n")
+print("\n\n\n\nTesting class CashDesk")
 
 values = [10, 20, 50, 100, 100, 100]
 bills = [Bill(value) for value in values]
@@ -37,3 +37,36 @@ desk.take_money(batch)
 desk.take_money(Bill(10))
 print(desk.total()) # 390
 desk.inspect()
+
+
+print("\n\n\n\nTesting functions draw_amount and draw_bills")
+print("\n\nCashDesk starting bills:")
+values = [50, 20, 100, 100, 50, 10, 10, 10, 10, 10, 5, 5]
+bills = [Bill(value) for value in values]
+batch = BillBatch(bills)
+desk = CashDesk()
+desk.take_money(batch)
+desk.inspect()
+
+print("\n\nAfter draw_amount(30)\n\nReturned amount:")
+returned_amount = desk.draw_amount(30)
+for bill in returned_amount:
+    print(bill)
+
+print("\n\nAfter draw_amount(125)\n\nReturned amount:")
+returned_amount = desk.draw_amount(125)
+for bill in returned_amount:
+    print(bill)
+
+print("\n\nCashDesk left bills:")
+desk.inspect()
+
+print("\n\nAfter draw_bills({10: 2, 5: 1})\n\nReturned amount:")
+returned_amount = desk.draw_bills({10: 2, 5: 1})
+for bill in returned_amount:
+    print(bill)
+
+print("\n\nCashDesk left bills:")
+desk.inspect()
+
+#desk.draw_bills({10: 2, 5: 1})  ValueError: Not enough bills of 10
--- Homework_05/Exercise_01_classes.py
+++ Homework_05/Exercise_01_classes.py
@@ -16,8 +16,8 @@ class Bill:
     def __int__(self):
         return self.__bill
 
-    def __eq__(self, otherObject):
-        return True if otherObject.__bill == self.__bill else False
+    def __eq__(self, other_object):
+        return other_object.__bill == self.__bill
 
     def __hash__(self):
         return hash(self.__bill)
@@ -46,18 +46,22 @@ class CashDesk:
     def __init__(self):
         self.__total = {}
 
-    def __magic_function_bill(self, bill):
+    def __add_bill(self, bill):
         billValue = int(bill)
         if self.__total.get(billValue) == None:
             self.__total[billValue] = 0
         self.__total[billValue] += 1
         
+    def __remove_bills(self, bills):
+        for bill in bills:
+            self.__total[int(bill)] -= 1
+
     def take_money(self, money):
         if isinstance(money, BillBatch):
             for bill in money:
-                self.__magic_function_bill(bill)
+                self.__add_bill(bill)
         else:
-            self.__magic_function_bill(money)
+            self.__add_bill(money)
 
     def total(self):
         sum = 0
@@ -69,9 +73,42 @@ class CashDesk:
         for key, value in self.__total.items():
             print("{}$ bills - {}".format(key, value))
 
-    def withdraw(self, sum):
+    def draw_amount(self, sum):
         if self.total() < sum:
             raise ValueError("Not enough cash")
+
+        bills = []
+        for key, value in self.__total.items():
+            if sum == 0:
+                break
+
+            if key <= sum:
+                for i in range(0, value):
+                    bills.append(Bill(key))
+                    sum -= key
+
+                    if key > sum:
+                        break 
+
+        self.__remove_bills(bills)
+        return BillBatch(bills)
+
+    def draw_bills(self, bills_dictionary):
+        bills = []
+        for key, value in bills_dictionary.items():
+            if self.__total.get(key) != None and self.__total[key] >= value:
+                for i in range(0, value):
+                    bills.append(Bill(key))
+            else:
+                raise ValueError("Not enough bills of {}".format(key))
+
+        self.__remove_bills(bills)
+        return bills
+                
+
+    
+
+        
 
         
         
